cc658d188345955c3750c68cbca31bb93570391b
Kartik Makhloga <90948334+KartikMakhloga@users.noreply.github.com>
2023-08-24 22:32:59 +0530

    added error handler function

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,9 +1,17 @@
 <script setup>
 const route = useRoute()
-const {data} = useFetch(`http://www.omdbapi.com/?apikey=39716fd&i=${route.params.id}`,{
-    pick:["Plot","Title"],
-    key:`/movies/$route.params.id`
+const { data,error } = await useFetch(`http://www.omdbapi.com/?apikey=39716fd&i=${route.params.id}`, {
+    pick: ["Plot", "Title","Error"],
+    key: `/movies/$route.params.id`,
 })
+
+if(error.value){
+  //we can handle error here 
+}
+
+if (data.value.Error === "Incorrect IMDb ID.") {
+            showError({ statusCode: 404, statusMessage: "Page Not Found" })
+        }
 </script>
 
 <template>
