48aa242de11f69efc7c82418dc06958e70794600
Kartik Makhloga <90948334+KartikMakhloga@users.noreply.github.com>
2023-08-25 12:02:52 +0530

    changes in head tag using useHead

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunk)=>{
+    return titleChunk ? `${titleChunk} - Movie App` : 'Movie App'
+  }
+})
+</script>
+
 <template>
   <div>
     <nav style="display:flex; gap:10px;">
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,17 +1,27 @@
 <script setup>
 const route = useRoute()
-const { data,error } = await useFetch(`http://www.omdbapi.com/?apikey=39716fd&i=${route.params.id}`, {
-    pick: ["Plot", "Title","Error"],
+const { data, error } = await useFetch(`http://www.omdbapi.com/?apikey=39716fd&i=${route.params.id}`, {
+    pick: ["Plot", "Title", "Error","Poster"],
     key: `/movies/$route.params.id`,
 })
 
-if(error.value){
-  //we can handle error here 
+if (error.value) {
+    //we can handle error here 
 }
 
 if (data.value.Error === "Incorrect IMDb ID.") {
-            showError({ statusCode: 404, statusMessage: "Page Not Found" })
-        }
+    showError({ statusCode: 404, statusMessage: "Page Not Found" })
+}
+
+useHead({
+    title: data.value.Title,
+    meta: [
+        { name: "description", content: data.value.Plot },
+        { property: "og:description", content: data.value.Plot },
+        { property: "og:image", content: data.value.Poster },
+        { name: "twitter:card", content: `summary_large_image` },
+    ],
+})
 </script>
 
 <template>
